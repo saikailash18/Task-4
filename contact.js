d6453b98824f0f0b785a07aa6b64931dfa649f34
Sai Kailash Yanamandra <saikailash.yanamandra@healthonus.io>
2023-11-07 17:24:27 +0530

    Final Changes

--- contact.js
+++ contact.js
@@ -1,73 +1,70 @@
-window.onload = function () {
-  fetch("states.json")
-    .then((response) => response.json())
-    .then((data) => {
-      const stateDropdown = document.getElementById("stateDropdown");
-      const promoCodeField = document.getElementById("code");
-      for (const stateCode in data) {
-        if (data.hasOwnProperty(stateCode)) {
-          const option = document.createElement("option");
-          option.value = stateCode;
-          option.text = data[stateCode];
-          stateDropdown.appendChild(option);
-        }
+fetch("states.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const stateDropdown = document.getElementById("stateDropdown");
+    const promoCodeField = document.getElementById("code");
+    for (const stateCode in data) {
+      if (data.hasOwnProperty(stateCode)) {
+        const option = document.createElement("option");
+        option.value = stateCode;
+        option.text = data[stateCode];
+        stateDropdown.appendChild(option);
       }
-      stateDropdown.onclick = function () {
-        const selectedState = stateDropdown.value;
-        if (selectedState) {
-          promoCodeField.value = data[selectedState] + " - PROMO";
-        } else {
-          promoCodeField.value = "";
-        }
-      };
-    })
-    .catch((error) => console.error("Error loading JSON data:", error));
-
-  const sendMessageButton = document.querySelector(".bottombuttons .Message");
-  const nameInput = document.getElementById("name");
-  const emailInput = document.getElementById("email");
-  const orgInput = document.getElementById("org");
-
-  sendMessageButton.onclick = function (event) {
-    if (
-      nameInput.value.trim() === "" ||
-      emailInput.value.trim() === "" ||
-      orgInput.value.trim() === ""
-    ) {
-      const errorElement = document.getElementById("nameError");
-      if (nameInput.value.trim() === "") {
-        errorElement.textContent = "Name is Required";
+    }
+    stateDropdown.onclick = function () {
+      const selectedState = stateDropdown.value;
+      if (selectedState) {
+        promoCodeField.value = data[selectedState] + " - PROMO";
       } else {
-        errorElement.textContent = "";
+        promoCodeField.value = "";
       }
+    };
+  })
+  .catch((error) => console.error("Error loading JSON data:", error));
 
-      const errorElement1 = document.getElementById("emailError");
-      if (emailInput.value.trim() === "") {
-        errorElement1.textContent = "Email is Required";
-      } else {
-        errorElement1.textContent = "";
-      }
+const sendMessageButton = document.querySelector(".bottombuttons .Message");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const orgInput = document.getElementById("org");
 
-      const errorElement2 = document.getElementById("orgError");
-      if (orgInput.value.trim() === "") {
-        errorElement2.textContent = "Org Name is Required";
-      } else {
-        errorElement2.textContent = "";
-      }
-      event.preventDefault();
+sendMessageButton.onclick = function (event) {
+  if (
+    nameInput.value.trim() === "" ||
+    emailInput.value.trim() === "" ||
+    orgInput.value.trim() === ""
+  ) {
+    const errorElement = document.getElementById("nameError");
+    if (nameInput.value.trim() === "") {
+      errorElement.textContent = "Name is Required";
     } else {
-      const errorElement = document.getElementById("nameError");
       errorElement.textContent = "";
+    }
 
-      const errorElement1 = document.getElementById("emailError");
+    const errorElement1 = document.getElementById("emailError");
+    if (emailInput.value.trim() === "") {
+      errorElement1.textContent = "Email is Required";
+    } else {
       errorElement1.textContent = "";
+    }
 
-      const errorElement2 = document.getElementById("orgError");
+    const errorElement2 = document.getElementById("orgError");
+    if (orgInput.value.trim() === "") {
+      errorElement2.textContent = "Org Name is Required";
+    } else {
       errorElement2.textContent = "";
     }
-  };
-};
+    event.preventDefault();
+  } else {
+    const errorElement = document.getElementById("nameError");
+    errorElement.textContent = "";
 
+    const errorElement1 = document.getElementById("emailError");
+    errorElement1.textContent = "";
+
+    const errorElement2 = document.getElementById("orgError");
+    errorElement2.textContent = "";
+  }
+};
 const maleRadio = document.getElementById("gender-male");
 const femaleRadio = document.getElementById("gender-female");
 maleRadio.onclick = function () {

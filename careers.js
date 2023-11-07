90ddfc732635ff6b0eb920c664cded0cd6b8dac3
Sai Kailash Yanamandra <saikailash.yanamandra@healthonus.io>
2023-11-07 16:58:06 +0530

    alignments

--- careers.js
+++ careers.js
@@ -1,10 +1,6 @@
-// function displayResume() {
-//   const fileInput = document.getElementById("upload");
-//   const fileName = fileInput.files[0].name;
-//   const resumeFileName = document.getElementById("resumeFileName");
-//   resumeFileName.innerHTML = fileName;
-// }
 function displayResume() {
-  document.getElementById("resumeFileName").innerHTML =
-    document.getElementById("upload").files[0].name;
+  const fileInput = document.getElementById("upload");
+  const fileName = fileInput.files[0].name;
+  const resumeFileName = document.getElementById("resumeFileName");
+  resumeFileName.innerHTML = fileName;
 }
